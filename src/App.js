9927ddeba7a9ea109a75e219ef4f7629549a88d7
raytieu <ray.tieu@gmail.com>
2022-09-16 18:08:28 -0700

    version 1.0 complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>JZ's Recipes</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -14,5 +21,22 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
